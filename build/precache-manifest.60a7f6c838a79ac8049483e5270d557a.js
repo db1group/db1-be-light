5192e3269ba4c37a4845b9d886d68d919624c963
Larissa Pissurno <larissa.pissurno@db1.com.br>
2020-04-09 19:00:54 -0300

    First manual deploy

---
+++ build/precache-manifest.60a7f6c838a79ac8049483e5270d557a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee2ba193a6a301ec2ddba6a2c0bbd3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cab4f4144fa3e29aa45",
+    "url": "/static/css/main.c5a52c2f.chunk.css"
+  },
+  {
+    "revision": "b029df1d3842fb5b4326",
+    "url": "/static/js/2.a75f562e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a75f562e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cab4f4144fa3e29aa45",
+    "url": "/static/js/main.83a3e6fc.chunk.js"
+  },
+  {
+    "revision": "d5200b787e6155c7f973",
+    "url": "/static/js/runtime-main.385500ac.js"
+  },
+  {
+    "revision": "76931ac3d62bae794a84c9b27280ce1e",
+    "url": "/static/media/logo.76931ac3.png"
+  }
+]);
